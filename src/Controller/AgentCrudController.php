4ee6cf09425af0df8c0c33ce401e767c4a0c9535
Anett David <davidanett@gmail.com>
2021-07-14 19:31:49 +0200

    Handle Agent form and display data

--- src/Controller/AgentCrudController.php
+++ src/Controller/AgentCrudController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Entity\Agent;
 use App\Form\AgentType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,12 +15,24 @@ class AgentCrudController extends AbstractController
     /**
      * @Route("/agent/creation", name="agent_create")
      */
-    public function agent_create(): Response
+    public function agent_create(Request $request, EntityManagerInterface $manager): Response
     {
-
         $agent = new Agent();
 
         $form = $this->createForm(AgentType::class, $agent);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+
+            $manager->persist($agent);
+            $manager->flush();
+
+            $this->addFlash('success', 'Un nouvel agent a été ajouté avec succès');
+
+            return $this->redirectToRoute('admin_agents');
+
+        }
 
         return $this->render('agent_crud/agent_create.html.twig', [
             'title' => 'Agents',
