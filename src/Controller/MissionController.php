2c5e04daba03da275f8869a2a6dd3b27ea8437a3
Anett David <davidanett@gmail.com>
2021-07-31 12:41:00 +0200

    Delete some unused templates

--- src/Controller/MissionController.php
+++ src/Controller/MissionController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Mission;
+use App\Repository\MissionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,10 +14,10 @@ class MissionController extends AbstractController
     /**
      * @Route("/", name="missions")
      */
-    public function index(): Response
+    public function index(MissionRepository $missionRepository): Response
     {
         return $this->render('mission/index.html.twig', [
-            'controller_name' => 'MissionController'
+            'missions' => $missionRepository->findAll()
         ]);
     }
 }
